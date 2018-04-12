758861dae8f88a29e5716b74c9b728cb2ea1a5b7
Elijah <etrexler8@gatech.edu>
2018-04-12 17:01:51 -0400

    Add database script

---
+++ database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'academic-mysql.cc.gatech.edu',
+  user: 'cs4400_Group_37',
+  password: 'tg7zvTd5',
+  database: 'cs4400_Group_37'
+});
+
+connection.query(
+    'SELECT * FROM `User`;',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+    //   console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
