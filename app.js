652c5947793ce86fd861b130ab3ba55146dc2386
luis-gzz <gamesbyluis@gmail.com>
2018-04-15 23:12:40 -0400

    Registration should now fully work for both visitors and owners

--- app.js
+++ app.js
@@ -60,11 +60,20 @@ app.post('/login', function(req, res){
 
 app.post('/registration', function(req, res){
   // === What it expects ===
-  // var payload = {
-  //   email = "...",
-  //   username = "...",
-  //   password = "...",
-  //   type = "..."
+  // let payload = {
+  //  "email": "test2@harvard.edu",
+  //  "username": "testUser2",
+  //  "password": "shgshYYVSH7",
+  //  "type" : "Owner",
+  //  "property": "Test Garden",
+  //  "address": "555 Test Road",
+  //  "city": "Atlanta",
+  //  "zip": "30332",
+  //  "acres": 2,
+  //  "public": 1,
+  //  "commercial": 0,
+  //  "propType": "Farm",
+  //  "items" : [["Pig"], ["Peruvian Lily"]]
   // }
   // === what it responds ===
   // "failed" for error failure
@@ -75,32 +84,89 @@ app.post('/registration', function(req, res){
   var password = req.body.password;
   var username = req.body.username;
   var type = req.body.type;
+  var email = req.body.email;
 
+  var id = Math.floor(Math.random()*90000) + 10000;
+  var propName = req.body.property;
+  var address = req.body.address;
+  var city = req.body.city;
+  var zip = req.body.zip;
+  var acres = req.body.acres;
+  var propType = req.body.propType;
+  var public = req.body.public;
+  var commercial = req.body.commercial;
+  var items = req.body.items;
+  for (i = 0; i < items.length; i++) {
+    items[i].unshift(id);
+  }
+
+console.log(items);
   // A query like login to check if users already exists
   connection.query(
       'SELECT Email, Username FROM `User` WHERE Email = ? OR Username = ?', [email, username],
       function(err, results, fields) {
-        console.log(results);
         if (results.length > 0) {
           // failure because the user already exists
           res.write("exists");
           res.end()
         } else {
+          // Insert the new user
           connection.query(
             'INSERT INTO User(Username, Email, Password, UType) VALUES (?, ?, ?, ?)', [username, email, password, type],
             function(err, results, fields) {
-              console.log(err)
               if (err == null) {
                 // Success the new entry was added
-                res.write("1");
+                if (type == "Owner") {
+                    // =======================
+                    // Succces adding owner so add new property
+                    connection.query(
+                    'INSERT INTO Property(ID, Name, Size, Address, IsPublic, IsCommercial, OwnedBy, PType) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [id, propName, acres, address + city + zip, public, commercial, email, propType],
+                    function(err, results, fields) {
+                      console.log(err)
+
+                      if (err == null) {
+                        connection.query(
+                        'INSERT INTO Grows_Raises(PId, IName) VALUES ?', [items],
+                        function(err, results, fields) {
+                          console.log(err)
+                          if (err == null) {
+                            // Success the new entry was added to property
+                           res.write("1");
+                          res.end()
+
+                          } else {
+                            // failed due to error adding property
+                            res.write("failed to add crop");
+                            res.end()
+                          }
+
+
+                        });
+
+                      } else {
+                        // failed due to error adding property
+                        res.write("failed to add property");
+                        res.end()
+                      }
+
+
+                    });
+
+                } else {
+                  //Success added visitor
+                  res.write("1");
+                  res.end()
+                }
+
               } else {
                 // failed do to error
-                res.write("failed");
+                res.write("failed to add user");
+                res.end()
               }
-              res.end()
+
 
           });
-        };
+        }
     });
 
 });
